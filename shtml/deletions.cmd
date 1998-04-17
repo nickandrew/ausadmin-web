34bfd00af3798eefc2c815aef19e2223de6b3a11
Nick Andrew <nick@nick-andrew.net>
1998-04-17 15:58:23 +0000

    Do the right thing if there are no deletions to do
    git-svn-id: https://svn.local.tull.net/svn/ausadmin-web/trunk@19 fadd3961-8b1f-0410-a1fe-b4cff5c9d479

--- shtml/deletions.cmd
+++ shtml/deletions.cmd
@@ -1,6 +1,13 @@
 #!/usr/bin/perl
+#	Emit a series of links to newsgroups scheduled for deletion
 
 chdir "Deletions";
+$count = 0;
 for $i (<*>) {
 	print "<li><a href=\"Deletions/$i\">$i</a>\n";
+	$count++;
+}
+
+if ($count == 0) {
+	print "<li>None at present\n";
 }
