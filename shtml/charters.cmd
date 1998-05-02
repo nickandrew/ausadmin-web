1fd05ca990fdde3571493c9fd0d6f5ce6bce3193
Nick Andrew <nick@nick-andrew.net>
1998-05-02 00:33:22 +0000

    Parse file and understand link format
    git-svn-id: https://svn.local.tull.net/svn/ausadmin-web/trunk@28 fadd3961-8b1f-0410-a1fe-b4cff5c9d479

--- shtml/charters.cmd
+++ shtml/charters.cmd
@@ -1,6 +1,43 @@
 #!/usr/bin/perl
+#	Emit a series of links to newsgroups charters
 
 chdir "Charters";
-for $i (<*>) {
-	print "<li><a href=\"Charters/$i\">$i</a>\n";
+# for $i (<*>) {
+# 	print "<li><a href=\"Charters/$i\">$i</a>\n";
+# }
+
+$count = 0;
+for $fn (<*>) {
+	open(F, "<$fn") || next;
+	$count++;
+	undef %data;
+
+	while (<F>) {
+		chop;
+		last if (/^$/);
+		($key, $value) = ($_ =~ /([^:]*):\s+(.*)/);
+		last if ($key eq "");
+#		print "key: $key, value: /$value/\n";
+		$data{$key} = $value;
+	}
+
+	close(F);
+	$key = $fn;
+	if ($data{Link} eq "") {
+		print "<li><a href=\"Charters/$fn\">$fn</a>\n";
+		next;
+	}
+
+	$name = $fn;
+	if ($data{Name} ne "") {
+		$name = $data{Name};
+	}
+
+	print "<li><a href=\"$data{Link}\">$name</a>\n";
 }
+
+if ($count == 0) {
+	print "<li>None at present\n";
+}
+
+exit(0);
