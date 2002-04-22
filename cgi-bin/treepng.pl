7792894379bd1fc8cc4d38905d1ff8b6c8c0ee24
Nick Andrew <nick@nick-andrew.net>
2002-04-22 05:16:32 +0000

    Converted from treegif ... Use a PNG file instead (GD.pm gif support appears patchy)
    git-svn-id: https://svn.local.tull.net/svn/ausadmin-web/trunk@99 fadd3961-8b1f-0410-a1fe-b4cff5c9d479

---
+++ cgi-bin/treepng.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+#	treepng.pl - Output a png containing a linked newsgroups tree
+#
+#	$Revision$
+#	$Source$
+#	$Date$
+
+my $AUSADMIN_HOME = "/home/ausadmin";
+
+chdir "$AUSADMIN_HOME/data";
+if (! -f "ausgroups") {
+	print "Content-type: text/plain\n";
+	print "Status: 404 Missing aus.* newsgroup file\n\n";
+	exit(2);
+}
+
+@in = stat("ausgroups");
+@out = stat("grouptree.png");
+
+if (! -f "grouptree.png" || $in[9] > $out[9]) {
+	system "$AUSADMIN_HOME/bin/grouptree.pl";
+	if ($? > 0) {
+		print "Content-type: text/plain\n";
+		print "Status: 500 Generator broken\n\n";
+		exit(2);
+	}
+}
+
+print "Content-type: image/png\n";
+print "\n";
+
+open(F, "<grouptree.png") || die "Unable to open grouptree.png $!";
+while (<F>) {
+	print $_;
+}
+close(F);
+exit(0);
