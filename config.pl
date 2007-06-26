5ce24da5a165431922874676c5469fd751bebda9
Nick Andrew <ausadmin@aus.news-admin.org>
2007-06-26 07:02:48 +0000

    Initial
    git-svn-id: https://svn.local.tull.net/svn/ausadmin-web/tags/stable-20061010@165 fadd3961-8b1f-0410-a1fe-b4cff5c9d479

---
+++ config.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+push(@INC, "/home/ausadmin/perllib");
+$ENV{AUSADMIN_WEB} = "/var/www/aus.news-admin.org";
+$ENV{AUSADMIN_HOME} = "/home/ausadmin";
+$ENV{AUSADMIN_DATA} = "/home/ausadmin/data";
+$ENV{AUSADMIN_HIER} = "aus";
+
+if ($ENV{HTTP_HOST} =~ /^([^.]+)\./) {
+	my $hier = $1;
+
+	$ENV{AUSADMIN_HIER} = $hier;
+	$ENV{AUSADMIN_DATA} = "/home/ausadmin/data/$hier.data";
+
+	if (! -d $ENV{AUSADMIN_DATA}) {
+		print qq{Content-Type: text/plain\n\nThere is no data on the news-admin.org website for $hier.*, sorry\n};
+		exit(8);
+	}
+}
+
+if (! -d $ENV{AUSADMIN_HOME}) {
+	die "No such directory: $ENV{AUSADMIN_HOME}";
+}
+
+1;
