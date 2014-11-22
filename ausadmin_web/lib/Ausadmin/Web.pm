e17e49a1f4657d23782e4eb725aff675174b7d12
Nick Andrew <nick@nick-andrew.net>
2014-11-23 01:52:03 +1100

    Tidy Ausadmin::Web

--- ausadmin_web/lib/Ausadmin/Web.pm
+++ ausadmin_web/lib/Ausadmin/Web.pm
@@ -1,25 +1,25 @@
+#!/usr/bin/perl
+
 package Ausadmin::Web;
+
 use Mojo::Base 'Mojolicious';
 
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+	my $self = shift;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+	# Router
+	my $r = $self->routes;
 
-  # Router
-  my $r = $self->routes;
+	# Normal route to controller
+	$r->get('/')->to('example#welcome');
+	$r->get('/groupinfo.cgi/*newsgroup')->to('newsgroup#groupinfo');
 
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
-  $r->get('/groupinfo.cgi/*newsgroup')->to('newsgroup#groupinfo');
-
-  $r->get('/checkgroups.shtml')->to(cb => sub {
-	my $c = shift;
-	$c->res->code(301);
-	$c->redirect_to('/checkgroups.txt');
-  });
+	$r->get('/checkgroups.shtml')->to(cb => sub {
+		my $c = shift;
+		$c->res->code(301);
+		$c->redirect_to('/checkgroups.txt');
+	});
 }
 
 1;
