4aea6ab2d4e3693c32763838531dceec04c49e56
Nick Andrew <nick@nick-andrew.net>
2014-11-24 02:07:58 +1100

    Cut out the stuff that requires an NNTP server
    That can go into some other class, later.

--- ausadmin_web/lib/Ausadmin/Web/Newsgroup.pm
+++ ausadmin_web/lib/Ausadmin/Web/Newsgroup.pm
@@ -8,13 +8,10 @@ Ausadmin::Web::Newsgroup - a newsgroup
 
  use Ausadmin::Web::Newsgroup;
 
- $nntp = new Net::NNTP(...)
-
- $ng = new Newsgroup(name => 'aus.history', [nntp_server=>$nntp], hier => $hier);
+ $ng = new Newsgroup(name => 'aus.history', hier => $hier);
  $bool = Newsgroup::valid_name('aus.history');
  $bool = Newsgroup::validate('aus.history');
 
- $ng->set_server($nntp)
  $flags = $ng->group_flags()  ... return the 'y' or 'm' status of the group
  
  $string = $ng->get_attr('charter') ... Read the charter data, return as string
@@ -93,59 +90,6 @@ sub validate {
 
 	return 1 if ($ng_name =~ /^[a-z0-9+-]+\.[a-z0-9+-]+(\.[a-z0-9+-]+)*(:\d\d\d\d-\d\d-\d\d)?$/);
 	return 0;
-}
-
-# Set the open connection to an NNTP server
-
-sub set_server {
-	my $self = shift;
-	my $conn = shift;
-
-	die "Newsgroup::set_server must be given a reference" if (!ref $conn);
-
-	$self->{nntp_server} = $conn;
-
-	return $conn;
-}
-
-# ---------------------------------------------------------------------------
-# Get newsgroup info. Return a hash ref: art_high, art_low, flags
-# ---------------------------------------------------------------------------
-
-sub group_info {
-	my $self = shift;
-
-	die "Newsgroup::group_info() called without a server set" if (!ref $self->{nntp_server});
-
-	my $name = $self->{name};
-
-	my $ref = $self->{nntp_server}->active($name);
-
-	return undef if (!ref $ref);
-
-	my $list = $ref->{$name};
-
-	$self->{group_info} = {
-		art_high => $list->[0],
-		art_low => $list->[1],
-		flags => $list->[2]
-	};
-
-	return $ref->{$self->{name}};
-}
-
-# ---------------------------------------------------------------------------
-# return 'y' for unmoderated, or 'm' for moderated group
-# ---------------------------------------------------------------------------
-
-sub group_flags {
-	my $self = shift;
-
-	if (!exists $self->{group_info}) {
-		$self->group_info();
-	}
-
-	return $self->{group_info}->{flags};
 }
 
 # ---------------------------------------------------------------------------
